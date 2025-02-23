edfa3a95cedb98c2d0f07200285d8e803073b893
Matthew Nicol <mjnicol92@gmail.com>
2025-02-23 17:43:17 +0000

    getting database server ready

--- Database/makeUser.php
+++ Database/makeUser.php
@@ -1,18 +1,25 @@
 #!/usr/bin/php
 <?php
 
+if ($argc < 4){
+	echo "requires username, eamil, and password.";
+	exit(1);
+}
+
+$username = $argv[1];
+$email = $argv[2];
+$password = $argv[3];
 
 $mydb = new mysqli('127.0.0.1','TeamDog123','TeamDog123','movDB');
 
-if ($mydb->errno != 0)
-{
+if ($mydb->errno != 0){
         echo "failed to connect to database: ". $mydb->error . PHP_EOL;
         exit(0);
 }
 
 echo "successfully connected to database".PHP_EOL;
 
-$query = "select * from users;";
+$query = "insert into users (username, email, password) values ($username, $email, $password);"
 
 $response = $mydb->query($query);
 if ($mydb->errno != 0)
@@ -23,14 +30,14 @@ if ($mydb->errno != 0)
 }
 
 if ($response->num_rows > 0){
-	while ($row = $response->fetch_assoc()){
-		print_r($row);
-		echo PHP_EOL;
-	}
+        while ($row = $response->fetch_assoc()){
+                print_r($row);
+                echo PHP_EOL;
+        }
 } else {
-	echo "No records found." . PHP_EOL;
+        echo "No records found." . PHP_EOL;
 }
 
 $mydb->close();
 ?>
-~        
+
